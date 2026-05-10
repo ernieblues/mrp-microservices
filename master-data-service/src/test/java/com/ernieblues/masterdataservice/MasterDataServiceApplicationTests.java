c39beab2067fc72c3889f1a8bc9ecb750cf10ff5
Ernest Guagliata <ernieblues@outlook.com>
2026-05-10 15:17:32 -0400

    init(master-data-service): Spring Boot project with Gradle Kotlin DSL

---
+++ master-data-service/src/test/java/com/ernieblues/masterdataservice/MasterDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernieblues.masterdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
