c052d3fbfdaa27f646cbbcf3a0d1f45c8f9f7ab0
Ernest Guagliata <ernieblues@outlook.com>
2026-05-13 00:44:19 -0400

    chore(master-data-service): add cost_centers and vendors seed data

---
+++ master-data-service/src/main/java/com/ernieblues/masterdataservice/config/DataSeeder.java
@@ -0,0 +1,48 @@
+package com.ernieblues.masterdataservice.config;
+
+import com.ernieblues.masterdataservice.entity.CostCenter;
+import com.ernieblues.masterdataservice.entity.Vendor;
+import com.ernieblues.masterdataservice.repository.CostCenterRepository;
+import com.ernieblues.masterdataservice.repository.VendorRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile({"local"})
+public class DataSeeder {
+
+    @Bean
+    CommandLineRunner seedData(
+            CostCenterRepository costCenterRepository,
+            VendorRepository vendorRepository)
+    {
+        return args -> {
+
+            if (costCenterRepository.count() == 0) {
+
+                List<CostCenter> costCenters = List.of(
+                        new CostCenter("ENG", "Engineering"),
+                        new CostCenter("IT", "Information Technology"),
+                        new CostCenter("MFG", "Manufacturing")
+                );
+
+                costCenterRepository.saveAll(costCenters);
+            }
+
+            if (vendorRepository.count() == 0) {
+
+                List<Vendor> vendors = List.of(
+                        new Vendor("ACME", "Acme Corporation", "sales@acme.com"),
+                        new Vendor("GIS", "Global Office Supply", "sales@gis.com"),
+                        new Vendor("PPC", "Precision Parts Co.", "sales@ppc.com")
+                );
+
+                vendorRepository.saveAll(vendors);
+            }
+        };
+    }
+}
