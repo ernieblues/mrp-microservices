84a8b634fb3676ca8f9cea688081d9b77ea8247d
Ernest Guagliata <ernieblues@outlook.com>
2026-05-14 13:15:07 -0400

    init(purchase-requisition-service): Spring Boot project with Gradle Kotlin DSL

---
+++ purchase-requisition-service/src/main/java/com/ernieblues/purchaserequisitionservice/PurchaseRequisitionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ernieblues.purchaserequisitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseRequisitionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseRequisitionServiceApplication.class, args);
+    }
+
+}
---
+++ purchase-requisition-service/src/test/java/com/ernieblues/purchaserequisitionservice/PurchaseRequisitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernieblues.purchaserequisitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseRequisitionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
